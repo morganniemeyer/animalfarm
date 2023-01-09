1d7c7728057265c0aafddeb0d24a4cbb557fa89a
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-09 16:38:11 -0600

    animals rendering

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,7 @@ export default function Main() {
   return (
     <main className="main" style={{ backgroundImage: `url(${background})` }}>
       {animals.map((animal) => (
-        <Animal key={animal.type} image="{animal.type}.svg" name={animal.name} says={animal.says} />
+        <Animal key={animal.type} image={animal.type} name={animal.name} says={animal.says} />
       ))}
     </main>
   );
