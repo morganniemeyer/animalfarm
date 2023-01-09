3595d1c28539416a5adfbe18dbe7d4c6566540d6
Morgan Niemeyer <malineniemeyer@gmail.com>
2023-01-09 16:11:27 -0600

    set up mapping in main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
+import animals from '../../data.js';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main className="main" style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main className="main" style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal key={animal.type} image="{animal.type}.svg" name={animal.name} says={animal.says} />
+      ))}
+    </main>
+  );
 }
